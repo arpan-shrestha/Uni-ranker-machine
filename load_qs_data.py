94ff8332cf5c0f996d6583066b9ba9a33e37bde3
Arpan Shrestha <arpanshrestha12@gmail.com>
2026-06-25 22:20:17 +0545

    Replaced string hyphens or whitespace hyphens with NaN and forced column to be numeric. Also SQL schema alteration

--- load_qs_data.py
+++ load_qs_data.py
@@ -1,5 +1,6 @@
 #column renaming and data insertion into postgresql database
 import pandas as pd
+import numpy as np
 df = pd.read_excel("2027 QS World University Rankings 1.1 (For qs.com).xlsx", header = 1)
 df.columns = [
     "index",
@@ -33,19 +34,40 @@ df.columns = [
     "overall_score"
 ]
 df = df.drop(columns=["index"])
+# drop duplicate text header row
+df = df[df["rank_2027"] != "Rank"]
+# Convert string hyphen into real numeric NULLS
+score_columns = [
+    "ar_score",
+    "er_score",
+    "fsr_score",
+    "cpf_score",
+    "ifr_score",
+    "isr_score",
+    "irn_score",
+    "eo_score",
+    "sus_score",
+    "overall_score"
+]
+for col in score_columns:
+    df[col] = df[col].replace(r"^\s*-\s*$", np.nan, regex=True) 
+    df[col] = pd.to_numeric(df[col],errors="coerce")
+
 print("Dataframe created successfully!!!")
 
 
 #connection
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 engine = create_engine(
     "postgresql+psycopg2://arpanshrestha:@localhost:5432/qs_rankings"
 )
+with engine.begin() as conn:
+    conn.execute(text("TRUNCATE TABLE universities;"))
 
 df.to_sql(
     "universities",
     engine,
-    if_exists="replace",
+    if_exists="append",
     index=False,
 )
 print("Data inserted successfully!!! Total rows inserted: ", len(df))
